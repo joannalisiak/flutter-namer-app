f9019a6198544102c42878066f8c96ac000ea19f
Joanna <joanna.lisiak13@gmail.com>
2023-11-20 17:28:17 +0100

    add favourites page

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
       case 0:
         page = GeneratorPage();
       case 1:
-        page = Placeholder();
+        page = FavouritesPage();
       default:
         throw UnimplementedError("no widget for $selectedIndex");
     }
@@ -94,6 +94,39 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       );
     });
+  }
+}
+
+class FavouritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var favourites = appState.favourites;
+
+    var theme = Theme.of(context);
+    var styleLarge = theme.textTheme.headlineSmall!
+        .copyWith(color: theme.colorScheme.onBackground);
+    var styleSmall = theme.textTheme.bodyMedium!
+        .copyWith(color: theme.colorScheme.onBackground);
+
+    if (favourites.isEmpty) {
+      return Center(child: Text("No favourites yet", style: styleLarge));
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(12),
+          child: Text("You have ${favourites.length} favourites:",
+              style: styleSmall),
+        ),
+        for (var favourite in favourites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(favourite.asLowerCase, style: styleSmall),
+          )
+      ],
+    );
   }
 }
 
