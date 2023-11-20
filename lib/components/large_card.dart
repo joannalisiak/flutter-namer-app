52ab689ed2484bc93db929dc2a7bdcdfd21f341f
Joanna <joanna.lisiak13@gmail.com>
2023-11-20 18:35:55 +0100

    improve large card text styling

--- lib/components/large_card.dart
+++ lib/components/large_card.dart
@@ -18,13 +18,19 @@ class LargeCard extends StatelessWidget {
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: pair.asPascalCase,
-        ),
-      ),
+          padding: const EdgeInsets.all(20),
+          child: AnimatedSize(
+            duration: Duration(milliseconds: 200),
+            child: MergeSemantics(
+                child: Wrap(
+              children: [
+                Text(pair.first,
+                    style: style.copyWith(fontWeight: FontWeight.w200)),
+                Text(pair.second,
+                    style: style.copyWith(fontWeight: FontWeight.bold))
+              ],
+            )),
+          )),
     );
   }
 }
